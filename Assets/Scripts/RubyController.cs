ee45cfc5f3599cdcfab0d2a356bdfb671b83c99c
E-SeongHo <lsh031255@naver.com>
2021-02-17 20:08:33 +0900

    Damage&Health

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    public float speed = 20.0f;
+    int currentHealth;
+    public int health
+    {
+        get
+        {
+            return currentHealth;
+        }
+    }
     Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
@@ -19,9 +32,14 @@ public class RubyController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 20.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 20.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
